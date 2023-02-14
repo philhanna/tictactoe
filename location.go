78fb9e82fe36d0342ef825743549fc02b2855fe0
Phil Hanna <ph1204@gmail.com>
2023-02-14 00:07:15 -0500

    Added ParseLocation() function to Location
    Fix for issue #3
    
    Squashed commit of the following:
    
    commit fd7169870e1a6f87b5e1d89c1f19ab461ba20769
    Author: Phil Hanna <ph1204@gmail.com>
    Date:   Tue Feb 14 00:06:23 2023 -0500
    
        Added unit tests
    
    commit 70fb690b17fd1c63432c1d7e45448bdf10e90d55
    Author: Phil Hanna <ph1204@gmail.com>
    Date:   Tue Feb 14 00:05:09 2023 -0500
    
        Test for negative numbers
    
    commit 193595983e6d28b10d3217a81986e94f6fad9a3f
    Author: Phil Hanna <ph1204@gmail.com>
    Date:   Mon Feb 13 23:52:24 2023 -0500
    
        Renamed method ParseLocation
    
    commit 0d400fe2dd56625621ac10b8fb8890075adf4e41
    Author: Phil Hanna <ph1204@gmail.com>
    Date:   Mon Feb 13 23:51:15 2023 -0500
    
        Added parse method to location

--- location.go
+++ location.go
@@ -1,6 +1,15 @@
 package tictactoe
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+// ---------------------------------------------------------------------
+// Type definitions
+// ---------------------------------------------------------------------
 
 // Location is a (row, col) pair of one of the squares of the board
 type Location struct {
@@ -8,8 +17,50 @@ type Location struct {
 	Col int
 }
 
+// ---------------------------------------------------------------------
+// Methods
+// ---------------------------------------------------------------------
+
 // String returns a string representation of the location
 func (l Location) String() string {
 	return fmt.Sprintf("(%d,%d)", l.Row, l.Col)
 }
 
+// ---------------------------------------------------------------------
+// Functions
+// ---------------------------------------------------------------------
+
+// ParseLocation gets a row and column number from a string. It verifies that:
+//	- There are two numbers in the string
+//	- Both are >= 0 and < 3
+// If any errors are detected, returns an error message.
+func ParseLocation(s string) (Location, error) {
+	re := regexp.MustCompile(`-?\d+`) // Allow for negative numbers
+	m := re.FindAllString(s, -1)
+	if m == nil {
+		errmsg := fmt.Sprintf("No numbers found in %q", s)
+		err := errors.New(errmsg)
+		return Location{}, err
+	}
+	if len(m) != 2 {
+		errmsg := fmt.Sprintf("Need 2 numbers; found %d in %q", len(m), s)
+		err := errors.New(errmsg)
+		return Location{}, err
+	}
+	row, _ := strconv.Atoi(m[0])
+	if row < 0 || row >= 3 {
+		errmsg := fmt.Sprintf("Row must be 0, 1, or 2, not %d", row)
+		err := errors.New(errmsg)
+		return Location{}, err
+	}
+	col, _ := strconv.Atoi(m[1])
+	if col < 0 || col >= 3 {
+		errmsg := fmt.Sprintf("Column must be 0, 1, or 2, not %d", col)
+		err := errors.New(errmsg)
+		return Location{}, err
+	}
+	
+	// Everything looks good
+	newLoc := Location{row, col}
+	return newLoc, nil
+}
--- location_test.go
+++ location_test.go
@@ -2,6 +2,7 @@ package tictactoe
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -11,5 +12,40 @@ func TestLocationString(t *testing.T) {
 	want := "(1,2)"
 	if have != want {
 		t.Errorf("have=%s,want=%s", have, want)
+	}
+}
+
+func TestParseLocation(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Location
+		wantErr bool
+	}{
+		{"Good numbers", args{"1,2"}, Location{1, 2}, false},
+		{"With parens", args{"(2, 2)"}, Location{2, 2}, false},
+		{"Row low", args{"(-1, 2)"}, Location{}, true},
+		{"Row high", args{"(3, 2)"}, Location{}, true},
+		{"Col low", args{"(1, -2)"}, Location{}, true},
+		{"Col high", args{"(2, 187)"}, Location{}, true},
+		{"Empty string", args{""}, Location{}, true},
+		{"Characters", args{"row,col"}, Location{}, true},
+		{"Partial", args{"1,col"}, Location{}, true},
+		{"More than 2 numbers", args{"1,2,3"}, Location{}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseLocation(tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseLocation() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseLocation() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
